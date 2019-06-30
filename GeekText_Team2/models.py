743aeff5af2b683bb934d255c375d593db6454da
guille117 <guillebarq@gmail.com>
2019-06-29 23:43:58 -0400

    Modifed DB and remade the homepage to look nice.

--- GeekText_Team2/models.py
+++ GeekText_Team2/models.py
@@ -17,30 +17,34 @@ from sqlalchemy.orm import sessionmaker
 ############ DATABASE MODELS ################
 #############################################
 
+
 @login_manager.user_loader
 def load_user(user_id):
 
     return User.query.get(user_id)
 
 ############### USER MODEL #############################
+
+
 class User(db.Model, UserMixin):
 
-    __tablename__ = 'users' #override tablename
+    __tablename__ = 'users'  # override tablename
     id = db.Column(db.Integer, primary_key=True)
-    profile_image = db.Column(db.String(25), nullable=False,default='default_profile.png')
+    profile_image = db.Column(
+        db.String(25), nullable=False, default='default_profile.png')
     name = db.Column(db.Text)
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
 
-    def __init__(self,name,email,username,password):
+    def __init__(self, name, email, username, password):
         self.name = name
         self.email = email
         self.username = username
         self.password_hash = generate_password_hash(password)
 
-    def check_password(self,password):
-        return check_password_hash(self.password_hash,password)
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return f"This is {self.name} with email -> {self.email}"
@@ -48,56 +52,47 @@ class User(db.Model, UserMixin):
 ########################################################
 
 ############### BOOK MODEL #############################
+
+
 class Book(db.Model):
 
     __tablename__ = 'books'
-    id =db.Column(db.Integer, primary_key=True,unique=True,nullable=False, autoincrement=True)
-    ISBN = db.Column(db.String(13),unique=False, nullable=True)
-    stock = db.Column(db.Integer)
-    author = db.Column(db.String(50))
-    publication_year = db.Column(db.String(4))
+    ISBN = db.Column(db.String(13), primary_key=True,
+                     unique=True, nullable=True)
     title = db.Column(db.Text, nullable=False)
-    #genre = db.Column(db.Text)
-    #price = db.Column(db.Numeric(10,2))
-
-    #pub_id = db.Column(db.Integer), nullable=False)
-    language_code = db.Column(db.String(10), nullable=False)
-
-    #auth_id = db.Column(db.Integer, db.ForeignKey('author.author_id'), nullable=False)
-    # AVERAGE OF 1-5
+    author = db.Column(db.Text)
+    genre = db.Column(db.Text)
+    publication_year = db.Column(db.String(4))
+    price = db.Column(db.Numeric(10, 2))
+    stock = db.Column(db.Integer)
+    description = db.Column(db.String(500))
     average_rating = db.Column(db.Float, nullable=False)
     ratings_count = db.Column(db.Integer, nullable=False)
-    #### TOTAL # OF EACH RATING ###
-    rating_1 = db.Column(db.Integer)
-    rating_2 = db.Column(db.Integer)
-    rating_3 = db.Column(db.Integer)
-    rating_4 = db.Column(db.Integer)
-    rating_5 = db.Column(db.Integer)
-    image_url = db.Column(db.String(150))
-    small_image_url = db.Column(db.String(150))
-    ###############################
-        # auth_id = db.relationship(db.Integer,'Author', backref='Book', lazy=True)
+    ratings_1 = db.Column(db.Integer)
+    ratings_2 = db.Column(db.Integer)
+    ratings_3 = db.Column(db.Integer)
+    ratings_4 = db.Column(db.Integer)
+    ratings_5 = db.Column(db.Integer)
+    image_url = db.Column(db.Text)
+    small_image_url = db.Column(db.Text)
 
-
-    def __init__(self,ISBN,stock,author,publication_year,title,language_code,average_rating,ratings_count,rating_1,rating_2,rating_3,rating_4,rating_5,image_url,small_image_url):
-        self.ISBN = ISBN
-        self.stock = stock
-        self.author = author
-        self.publication_year = publication_year
+    def __init__(self, title, author, genre, publication_year, price, stock, description, average_rating, ratings_count, ratings_1, ratings_2, ratings_3, ratings_4, ratings_5, image_url, small_image_url):
         self.title = title
-        self.language_code = language_code
+        self.author = author
+        self.genre = genre
+        self.publication_year = publication_year
+        self.price = price
+        self.stock = stock
+        self.description = description
         self.average_rating = average_rating
         self.ratings_count = ratings_count
-        self.rating_1 = rating_1
-        self.rating_2 = rating_2
-        self.rating_3 = rating_3
-        self.rating_4 = rating_4
-        self.rating_5 = rating_5
+        self.ratings_1 = ratings_1
+        self.ratings_2 = ratings_2
+        self.ratings_3 = ratings_3
+        self.ratings_4 = ratings_4
+        self.ratings_5 = ratings_5
         self.image_url = image_url
         self.small_image_url = small_image_url
-        #self.price = price
-
-
 
 
 ############### PUBLISHER MODEL #############################
@@ -118,14 +113,14 @@ class Book(db.Model):
 
 
 ############### AUTHOR MODEL #############################
-class Author(db.Model):
+# class Author(db.Model):
 
-    __tablename__ = 'author'
+ #   __tablename__ = 'author'
 
-    author_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Text, nullable=False)
+  #  author_id = db.Column(db.Integer, primary_key=True)
+   # name = db.Column(db.Text, nullable=False)
     #books = db.relationship('Book', backref='author', lazy=True)
 
-    def __init__(self, name):
-        self.name = name
+    # def __init__(self, name):
+     #   self.name = name
 ############################################################
