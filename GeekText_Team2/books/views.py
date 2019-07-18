a5aa53100592f1422895fee040ae003f225e8ab3
Elias Chacin <chacinelias@gmail.com>
2019-07-18 17:52:09 -0400

    added templates for each genre

--- GeekText_Team2/books/views.py
+++ GeekText_Team2/books/views.py
@@ -4,20 +4,20 @@ from GeekText_Team2.models import Book
 
 books_blueprint = Blueprint('books', __name__, template_folder='templates/books')
 
+
 @books_blueprint.route('/browse')
 def list():
-    books = Book.query.all()
-    return render_template('list.html', books=books)
+    sort_by = request.args.get('sort_by')
 
-    
-@books_blueprint.route('/browse/sortby/<string:sort_by>')
-def book_sort(sort_by):
-    books = Book.query.order_by(sort_by)
-    
+    if sort_by:
+        books = Book.query.order_by(sort_by)
+    else:
+        books = Book.query.all()
+
     return render_template('list.html', books=books)
 
 @books_blueprint.route('/browse/genre')
-def genre_filter():
+def genre():
     genre = request.args.get('genre')
 
     if genre == 'all':
@@ -25,4 +25,4 @@ def genre_filter():
     else:
         books = Book.query.filter_by(genre=genre)
 
-    return render_template('list.html', books=books)
+    return render_template(genre + '.html', books=books)
